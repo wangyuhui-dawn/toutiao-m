e539d487276b2fb1cca3e2028143ce327fccfbd3
Wangyuhui <1365183019@qq.com>
2020-12-02 16:08:33 +0800

    项目初始化-调整项目初始结构

--- src/main.js
+++ src/main.js
@@ -1,10 +1,14 @@
+/*项目主入口文件 webpack打包入口*/
 import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
 import store from './store'
-
+/*加载全局样式*/
+import './styles/index.less'
+/*商品提示*/
 Vue.config.productionTip = false
-
+/*创建Vue根实例*/
+/*把App根组件渲染到#app节点*/
 new Vue({
   router,
   store,
